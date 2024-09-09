fc62058262daf13e6c034d24a3fd0f2cf2d78360
bamcortezz <2022312593@dhvsu.edu.ph>
2024-09-09 18:44:35 +0800

    Cortez, Francis Emil M.

--- dashboard/admin/index.php
+++ dashboard/admin/index.php
@@ -1,27 +1,29 @@
-<?php 
-   require_once 'authentication/admin-class.php';
+<?php
+require_once 'authentication/admin-class.php';
 
-   $admin = new ADMIN();
+$admin = new ADMIN();
 
-   if (!$admin->isUserLoggedIn())
-   {
-      $admin -> redirect('../../');
-   }
+if (!$admin->isUserLoggedIn()) {
+  $admin->redirect('../../');
+}
 
-   $stmt = $admin->runQuery("SELECT * FROM user WHERE id = :id");
-   $stmt->execute(array(":id" => $_SESSION['adminSession']));
-   $user_data = $stmt->fetch(PDO::FETCH_ASSOC);
+$stmt = $admin->runQuery("SELECT * FROM user WHERE id = :id");
+$stmt->execute(array(":id" => $_SESSION['adminSession']));
+$user_data = $stmt->fetch(PDO::FETCH_ASSOC);
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-   <meta charset="UTF-8">
-   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>Admin Dashboard</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Admin Dashboard</title>
 </head>
+
 <body>
-   <h1>Welcome <?php echo $user_data['username'] ?></h1>
-   <button><a href="authentication/admin-class.php?admin_signout">Sign Out</a></button>
+  <h1>Welcome <?php echo $user_data['username'] ?></h1>
+  <button><a href="authentication/admin-class.php?admin_signout">Sign Out</a></button>
 </body>
+
 </html>
--- index.php
+++ index.php
@@ -5,45 +5,46 @@ include_once 'config/setting-configuration.php';
 <html lang="en">
 
 <head>
-   <meta charset="UTF-8">
-   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>Document</title>
-   <link rel="stylesheet" href="src/css/bootstrap.css">
-   <link rel="stylesheet" href="src/js/bootstrap.js">
-   <link rel="stylesheet" href="src/css/style.css">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="src/css/bootstrap.css">
+  <link rel="stylesheet" href="src/js/bootstrap.js">
+  <link rel="stylesheet" href="src/css/style.css">
 </head>
 
 <body>
 
-   <div class="wrapper">
-      <div class="container main">
-         <div class="row">
-            <div class="col-md-6 side-image">
-               <img src="././src/img/side-pic.jpg" alt="">
+  <div class="wrapper">
+    <div class="container main">
+      <div class="row">
+        <div class="col-md-6 side-image">
+          <img src="././src/img/side-pic.jpg" alt="">
+        </div>
+        <div class="col-md-6 right">
+          <form action="dashboard/admin/authentication/admin-class.php" method="POST">
+            <div class="input-box">
+              <h1>LOGIN</h1>
+              <div class="input-field">
+                <input type="hidden" name="csrf_token" value="<?php echo $csrf_token ?>">
+              </div>
+              <div class="input-field">
+                <input type="email" name="email" required class="form-control" placeholder="Enter Email">
+              </div>
+              <div class="input-field">
+                <input type="password" name="password" required class="form-control" placeholder="Enter Password">
+              </div>
+              <div class="input-field">
+                <button type="submit" name="btn-signin" class="submit">Sign in</button>
+              </div>
+              <div class="signin">
+                <span>Need to register? <a href="registration.php">Register here</a></span>
+              </div>
             </div>
-            <div class="col-md-6 right">
-               <form action="dashboard/admin/authentication/admin-class.php" method="POST">
-                  <div class="input-box">
-                     <h1>LOGIN</h1>
-                     <div class="input-field">
-                        <input type="hidden" name="csrf_token" value="<?php echo $csrf_token ?>">
-                     </div>
-                     <div class="input-field">
-                        <input type="email" name="email" required class="form-control" placeholder="Enter Email">
-                     </div>
-                     <div class="input-field">
-                        <input type="password" name="password" required class="form-control" placeholder="Enter Password">
-                     </div>
-                     <div class="input-field">
-                        <button type="submit" name="btn-signin" class="submit">Sign in</button>
-                     </div>
-                     <div class="signin">
-                        <span>Need to register? <a href="registration.php">Register here</a></span>
-                     </div>
-                  </div>
-               </form>
-            </div>
-         </div>
+          </form>
+        </div>
       </div>
-   </div>
+    </div>
+  </div>
+
 </html>
