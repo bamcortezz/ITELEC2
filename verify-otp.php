ba4053a0aa3e5200ae1a05f562eeffd55f2bb8d5
bamcortezz <2022312593@dhvsu.edu.ph>
2024-09-24 18:34:43 +0800

    Adding style to the verify-otp.php

--- verify-otp.php
+++ verify-otp.php
@@ -14,38 +14,34 @@ include_once 'config/setting-configuration.php';
 </head>
 
 <body>
-  <div class="wrapper">
   <div class="container d-flex align-items-center justify-content-center vh-100">
     <div class="row">
       <div class="col-md shadow p-3 mb-5 bg-body-tertiary rounded p-5">
-
+        <div class="text-center mb-4">
+          <img src="src/img/mail.jpg" alt="Mail Logo" style="width: 90px;">
+        </div>
         <div class="text-center mb-3">
-          <h1>Enter OTP</h1>
+          <h2 class="fw-bolder">OTP VERIFICATION</h2>
+        </div>
+        <div class="text-center mb-3">
+          <h6>We have sent a one time password (OTP) to your email</h6>
         </div>
         <form action="dashboard/admin/authentication/admin-class.php" method="post">
           <div class="input-box">
-
             <div class="form-group">
               <input type="hidden" name="csrf_token" value="<?php echo $csrf_token ?>">
             </div>
-
             <div class="form-group">
               <input type="number" name="otp" placeholder="Enter OTP" required class="form-control"><br>
             </div>
-
             <div class="form-group">
               <button type="submit" name="btn-verify" class="btn btn-primary w-100">Verify</button>
             </div>
-
           </div>
-
-
         </form>
-
       </div>
     </div>
   </div>
-</div>
 </body>
 
 </html>
