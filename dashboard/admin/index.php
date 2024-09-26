9416377c7c6eaffbccfe1dc35bc04af6bdf02814
bamcortezz <2022312593@dhvsu.edu.ph>
2024-09-26 11:28:25 +0800

    Adding style dashboard

--- dashboard/admin/index.php
+++ dashboard/admin/index.php
@@ -19,11 +19,23 @@ $user_data = $stmt->fetch(PDO::FETCH_ASSOC);
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Admin Dashboard</title>
+  <link rel="stylesheet" href="../../src/css/bootstrap.css">
+  <link rel="stylesheet" href="../../src/js/bootstrap.js">
 </head>
 
 <body>
-  <h1>Welcome <?php echo $user_data['username'] ?></h1>
-  <button><a href="authentication/admin-class.php?admin_signout">Sign Out</a></button>
+
+  <div class="shadow-sm p-3 mb-5 bg-body-tertiary rounded w-100 ">
+    <div class="container d-flex align-items-center justify-content-between">
+      <div>
+        <h1><?php echo $user_data['username'] ?></h1>
+      </div>
+      <div>
+        <button class="btn btn-warning"><a href="authentication/admin-class.php?admin_signout" class="text-decoration-none text-black">Sign Out</a></button>
+      </div>
+    </div>
+  </div>
+  
 </body>
 
 </html>
